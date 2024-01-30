e736f1a8b471d3133ab2bbfaa8c7cb7643bf5418
Swiatoslaw <swiatoslaw.p@gmail.com>
2024-01-30 11:37:40 +0100

    executed gdb several times

--- one/c.c
+++ one/c.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(void)
 {
-  fprintf(stdout, "Hello World!!");
+  for(register int i = 0; i < 10;++i)
+  {
+    printf("Some deep text.\n");
+  }
+  fprintf(stdout, "Hello World!!");//Something went wrong
+  char *pointer = (char *)malloc(sizeof(char) * 25ULL);
+  for(register int i = 0; i < 10;++i)
+  {
+    printf("Some wrong text.\n");
+  }
   return 0;
 }
